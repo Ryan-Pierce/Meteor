85d8fbb64490f8dd62d40a84354ac36d3d5c35af
RyanPierce <RyanPierce@gatech.edu>
2020-11-20 22:49:05 -0600

    block should have been a suspend fun

--- app/src/main/java/me/ryanpierce/trialanimations/MeteorFlow.kt
+++ app/src/main/java/me/ryanpierce/trialanimations/MeteorFlow.kt
@@ -1,11 +1,13 @@
 package me.ryanpierce.trialanimations
 
-import android.content.Context
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
 @OptIn(kotlinx.coroutines.InternalCoroutinesApi::class)
-suspend fun Flow<Meteor>.collect(location: Coordinate, block: (Meteor) -> Unit) = collect { meteor ->
+suspend fun Flow<Meteor>.collect(
+    location: Coordinate,
+    block: suspend (Meteor) -> Unit
+) = collect { meteor ->
     meteor.actor.send(location)
     delay(1000)
     block(meteor)
